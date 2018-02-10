eb094039f871b0e2ea24a905c53dca0a445e20f8
JoanChirinos <jchirinos@stuy.edu>
2018-02-09 20:28:36 -0500

    Fixed some python errors

--- make.py
+++ make.py
@@ -5,22 +5,21 @@ import cgi,cgitb
 cgitb.enable()
 
 def go():
-    print 'Content-type: text/html'
+    print 'Content-type: text/html\n\n'
     html = opensaveclose("itemPageTemplate.html")
-    vals = cgi.FieldStorage()
-    what = vals.getValue("what")
+    what = "Small_Pot"
     itemData = opensaveclose("itemFiles/items.csv").split("\n")
     for s in itemData:
-        if s[:len(what) == what]:
+        if s[:len(what)] == what:
             itemData = s.replace("_", " ").split(",")
             break
-    html.replace("ITEMNAME", itemData[0])
-    html.replace("ITEMPRICE", itemData[1])
-    html.replace("ITEMDETAILS", itemData[2])
-    html.replace("IMAGENAME", itemData[3])
+    html = html.replace("ITEMNAME", itemData[0])
+    html = html.replace("ITEMPRICE", itemData[1])
+    html = html.replace("ITEMDETAILS", itemData[2])
+    html = html.replace("IMAGENAME", itemData[3])
     print html
     
-def opensaveclose(fileName) {
+def opensaveclose(fileName):
     straw = open(fileName, "rU")
     data = straw.read()
     straw.close()
@@ -28,6 +27,5 @@ def opensaveclose(fileName) {
     straw.write(data)
     straw.close()
     return data
-}
 
 go()
